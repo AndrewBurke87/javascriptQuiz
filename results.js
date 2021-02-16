c7e10f3c9c46c6c290c3d90022b3910bda154786
Andrew Burke <Drewburke39@gmail.com>
2021-02-15 18:40:09 -0700

    added local storage

--- results.js
+++ results.js
@@ -4,5 +4,6 @@ var lastScore = localStorage.getItem("lastScore");
 var finalScore = localStorage.getItem("final");
 
 $("#score").text(finalScore)
-finalScore
-
+highScore.push(localScore)
+localStorage.setItem("highScore", finalScore)
+console.log(highScore)
