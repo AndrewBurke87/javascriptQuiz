e7536a35822e64bb3fadbed511b77d24e0aae36f
Andrew Burke <Drewburke39@gmail.com>
2021-02-17 22:08:18 -0700

    added prompt to add initials to local storage

--- results.js
+++ results.js
@@ -4,6 +4,7 @@ var lastScore = localStorage.getItem("lastScore");
 var finalScore = localStorage.getItem("final");
 
 $("#score").text(finalScore)
+var localScore = prompt("You scorced " + finalScore + "Enter your Initials")
 highScore.push(localScore)
 localStorage.setItem("highScore", finalScore)
 console.log(highScore)
